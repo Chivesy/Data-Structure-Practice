93588ee025d27254129853744f4174aaf45b2173
Joseph <lars4076@umn.edu>
2021-09-14 18:03:26 -0500

    Implemented StackAsLinkedList
    Implemented Stack as a Linked List StackAsLinkedList.java and updated the driver class Main.java to include basic tests for new implementation.

--- Main.java
+++ Main.java
@@ -1,20 +1,43 @@
 /**
- * Driver program to run basic stack data structure program.
+ * Driver program to run stack data structure programs. Array and Linked List implementations.
  */
-public class Main {
-    /** Main program to run basic stack data structure program.
+ public class Main {
+    /** Main program to run stack data structure programs. Array and Linked List implementations.
      *  @param args no arguments required.
      */
     public static void main(String[] args) {
+
+        // Testing the Array Implementation
+        System.out.println("Implementation of a Stack with an Array\n");
         Stack s = new Stack();
+        Boolean empty = s.isEmpty();
+        System.out.println("The Stack is currently empty: " + empty);
         s.push(10);
         s.push(20);
         s.push(30);
         s.pop();
         s.peek();
         s.print();
-        Boolean empty = s.isEmpty();
+        empty = s.isEmpty();
+        System.out.println("The Stack is currently empty: " + empty + "\n\n");
+
+        // Testing the Linked List Implementation
+        System.out.println("Implementation of a Stack with a Linked List\n");
+        StackAsLinkedList sall = new StackAsLinkedList();
+        empty = sall.isEmpty();
         System.out.println("The Stack is currently empty: " + empty);
+        sall.isEmpty();
+        sall.push(10);
+        sall.push(20);
+        sall.push(30);
+        sall.push(40);
+        sall.push(100);
+        sall.pop();
+        sall.pop();
+        sall.peek();
+        empty = sall.isEmpty();
+        System.out.println("The Stack is currently empty: " + empty);
+        sall.print();
     }
 }
 
---
+++ StackAsLinkedList.java
@@ -0,0 +1,94 @@
+/** 
+ * The class StackAsLinkedList to create and implement a stack data structure using Linked Lists
+ * @author Joseph Larson
+ */
+public class StackAsLinkedList {
+    /**
+     * The root is the highest item in the stack
+     */
+    private StackNode root;
+    static class StackNode {
+        int data;
+        StackNode next;
+
+        /**
+         * Constructor of a StackNode for linked list
+         * @param data integer value stored in a stack node.
+         */
+        StackNode(int data) {
+            this.data = data;
+        }
+    }
+
+    /**
+     * Checks if the stack is empty
+     * @return False: stack not empty. True: stack is empty
+     */
+    public boolean isEmpty(){
+        if(root == null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Adds an item to the stack linked list data structure.
+     * @param data integer value being added to stack
+     */
+    public void push(int data){
+        StackNode newNode = new StackNode(data);
+
+        if (root == null) {
+            root = newNode;
+        } else {
+            StackNode temp = root;
+            root = newNode;
+            newNode.next = temp;
+        }
+        System.out.println(data + " pushed onto the stack");
+    }
+
+    /**
+     * Removes an item from the stack linked list data structure.
+     * @return integer data value of the removed stack item.
+     */
+    public int pop(){
+        if(root == null){
+            System.out.println("Stack is currently Empty");
+            return Integer.MIN_VALUE;
+        } else {
+            int popped = root.data;
+            root = root.next;
+            return popped;
+        }
+    }
+
+    /**
+     * Looks at the current item at the top of the linked list data structure.
+     * @return integer data value of the top most item in the stack.
+     */
+    public int peek(){
+        if(root == null){
+            System.out.println("Stack is currently Empty");
+            return Integer.MIN_VALUE;
+        } else {
+            return root.data;
+        }
+    }
+
+    /**
+     * Prints out all the values currently in the linked list stack.
+     */
+    public void print() {
+        if(root == null){
+            System.out.println("Stack is currently Empty");
+        } else {
+            System.out.println("Elements currently present in Stack: ");
+            StackNode cur = root;
+            while (cur != null){
+                System.out.println(cur.data);
+                cur = cur.next;
+            }
+        }
+    }
+}
