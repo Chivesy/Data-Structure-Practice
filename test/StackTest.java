73578f3efc5c902a2084b20205f5ab663a1df969
Joseph <lars4076@umn.edu>
2021-09-22 20:14:10 -0500

    testPush() and testPop() implemented
    Two more test cases for Stack class.

--- test/StackTest.java
+++ test/StackTest.java
@@ -5,12 +5,30 @@ import org.junit.Test;
 public class StackTest {
     
     @Test 
-    public void TestIsEmpty(){
-        // Set up
+    public void testIsEmpty(){
         Stack myStack = new Stack();
         assertEquals(true, myStack.isEmpty());
 
         myStack.push(1);
         assertEquals(false, myStack.isEmpty());
+    }
+
+    @Test
+    public void testPush(){
+        Stack myStack = new Stack();
+        assertEquals(true, myStack.push(0));
+        for(int i = 1; i <= 999; i++){
+            myStack.push(i);
+        }
+        assertEquals(false, myStack.push(1000));
+    }
+
+    @Test
+    public void testPop(){
+        Stack myStack = new Stack();
+        assertEquals(0, myStack.pop());
+
+        myStack.push(5);
+        assertEquals(5, myStack.pop());
     }
 }
